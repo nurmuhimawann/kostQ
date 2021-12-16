9b0771cd563a8f6e1c9abdf8b3ead96fd08bf07e
varreldt <varreldtantio@gmail.com>
2021-12-16 19:39:06 +0700

    update main & loginModel

--- src/com/kostQ/Main.java
+++ src/com/kostQ/Main.java
@@ -1,8 +1,15 @@
 package com.kostQ;
 
+import com.kostQ.views.DashboardView;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        try {
+            DashboardView.menuAwal();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
